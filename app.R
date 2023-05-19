b313da3699b6f3fcc18b36631d5139e7a7556359
nambatu <julianlangschwert@gmail.com>
2023-05-19 17:44:47 +0200

    app.R cleaned

--- app.R
+++ app.R
@@ -5,7 +5,8 @@ rm(list = ls())
   if (!require("leaflet")) install.packages("leaflet"); library(leaflet)
   if (!require("gapminder")) install.packages("gapminder"); library(gapminder)
   if (!require("dplyr")) install.packages("dplyr"); library(dplyr)
-  # TODO: implement missing packages (sp, purrr?)
+  if (!require("geojsonio")) install.packages("geojsonio"); library(geojsonio)
+  if (!require("sp")) install.packages("sp"); library(sp)
 }
 
 
@@ -49,14 +50,16 @@ server <- function(input, output) {
     else if (input$data == "GDP per Capita") {
       map_data = "gdpPercap"}
     
-    input_data <- data.frame(test=data_Map %>% pluck(map_data))
+    #input_data <- data.frame(test=data_Map %>% pluck(map_data))
     
     Map <- leaflet(data_Map) %>% addTiles() %>% addPolygons()
     
-    bins <- c(10, 20, 40, 50, 60, 70, 80, 90, Inf) #thresholds
-    pal <- colorBin("Blues", domain = input_data$test, bins = bins) # color palette
+    
     names(data_Map)[names(data_Map) == map_data] <- "test"
-    head(data_Map)
+    
+    bins <- c(10, 20, 40, 50, 60, 70, 80, 90, Inf) #thresholds
+    pal <- colorBin("Blues", domain = data_Map$test, bins = bins) # color palette
+    
     labels <- sprintf(
       "<strong>%s</strong><br/>%s: %g",
       data_Map$name, input$data, data_Map$test) %>% lapply(htmltools::HTML)
