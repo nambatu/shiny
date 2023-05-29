826ee12569a2e1675aadb9e8c6de3fb80863c44f
nambatu <julianlangschwert@gmail.com>
2023-05-29 12:21:57 +0200

    old shiny_tutorial file deleted

--- shiny_tutorial.R
+++
@@ -1,9 +0,0 @@
-### R Shiny Tutorial ###
-
-{
-  if (!require("shiny")) install.packages("shiny"); library(shiny)
-  
-  if (!require("gapminder")) install.packages("gapminder"); library(gapminder)
-  
-  if (!require("dplyr")) install.packages("dplyr"); library(dplyr)
-}
