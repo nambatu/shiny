e9fcf9bcd5e3d03d4f06727f41bb5a2828de8131
sarajerkovic <sarajerkovic.sj98@gmail.com>
2023-06-23 16:46:43 +0200

    ...

--- app.R
+++ app.R
@@ -16,55 +16,63 @@ rm(list = ls())
 # define UI
 ui <- fluidPage(
   # TODO: implement Layout
-  textOutput("header"),
-  selectInput("data", "Data", c("Life Expectancy", "Population", "GDP per Capita")),
-  selectInput("year", "Year", distinct(gapminder, gapminder$year)),
-  submitButton("submit", "Apply Changes"),
-  leafletOutput("map")
-  )
+  
+  theme = bslib::bs_theme(version = 4, bootswatch = "minty"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      h1("Explore a dataset"),
+      selectInput("data", "Data", c("Life Expectancy", "Population", "GDP per Capita")),
+      selectInput("year", "Year", distinct(gapminder, gapminder$year))
+    ),
+    mainPanel(
+      h1("World Map"),
+      leafletOutput("map")
+    )
+  ),
+)
+
 
 
 # define Server
 server <- function(input, output) {
-  output$header <- renderText({
-    "Header Placeholder"
-  })
   
   output$map <- renderLeaflet({
-     # TODO: clean Code, personalize per chosen data (eg bins), add comments
-    
+    # filter by year
     gapminder_filtered <- gapminder %>% filter(year == input$year)
     
+    # read geojson file, filter and merge with gapminder data
     WorldCountry <- geojsonio::geojson_read("./countries.geo.json", what = "sp")
     data_Map <- WorldCountry[WorldCountry$name %in% gapminder_filtered$country, ]
-    
     require(sp) # the trick is that this package must be loaded!
     data_Map <- merge(data_Map, gapminder_filtered, by.x="name", by.y="country")
     
+    # define colour palettes for different data sets
     if (input$data == "Life Expectancy"){
       map_data = "lifeExp"
-      names(data_Map)[names(data_Map) == "lifeExp"] <- "test"}
+      bins <- c(20, 40, 50, 60, 70, 75, 80, 85) 
+      pal <- colorBin("Blues", domain = data_Map$temp, bins = bins) 
+      }
     else if (input$data == "Population") {
-      map_data = "pop"}
+      map_data = "pop"
+      bins <- c(10000, 1*10^6, 10*10^6, 50*10^6, 100*10^6, 200*10^6, 400*10^6, 600*10^6)
+      pal <- colorBin("Reds", domain = data_Map$temp, bins = bins) 
+      }
     else if (input$data == "GDP per Capita") {
-      map_data = "gdpPercap"}
+      map_data = "gdpPercap"
+      bins <- c(200, 1000, 5000, 10000, 20000, 30000, 40000, 50000)  # Can you find some errors in the data? (Hint: Look at early years in Middle East)
+      pal <- colorBin("Greens", domain = data_Map$temp, bins = bins)
+      }
+    names(data_Map)[names(data_Map) == map_data] <- "temp"  # rename current column so that we can access it easier
     
-    #input_data <- data.frame(test=data_Map %>% pluck(map_data))
-    
+    # leaflet stuff :)
     Map <- leaflet(data_Map) %>% addTiles() %>% addPolygons()
-    
-    
-    names(data_Map)[names(data_Map) == map_data] <- "test"
-    
-    bins <- c(10, 20, 40, 50, 60, 70, 80, 90, Inf) #thresholds
-    pal <- colorBin("Blues", domain = data_Map$test, bins = bins) # color palette
-    
     labels <- sprintf(
       "<strong>%s</strong><br/>%s: %g",
-      data_Map$name, input$data, data_Map$test) %>% lapply(htmltools::HTML)
+      data_Map$name, input$data, data_Map$temp) %>% lapply(htmltools::HTML)
     
     Map <- Map %>% addPolygons(
-      fillColor = ~pal(data_Map$test),
+      fillColor = ~pal(data_Map$temp),
       weight = 0.5,
       opacity = 1,
       color = "black",
